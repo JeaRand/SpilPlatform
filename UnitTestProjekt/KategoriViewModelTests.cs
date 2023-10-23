3d4afde019a6b43cea01c582326681e36eb2f88b
JeaRand <jwra42959@edu.ucl.dk>
2023-10-23 11:52:23 +0200

    Comment and UnitTest

---
+++ UnitTestProjekt/KategoriViewModelTests.cs
@@ -0,0 +1,42 @@
+﻿using SpilPlatform.Mvvm.ViewModels;
+using Xunit;
+
+namespace UnitTestProjekt
+{
+    public class KategoriViewModelTests
+    {
+        [Fact]
+        public void KategoriViewModel_KategorierContainsExpectedCategories()
+        {
+            // Arrange: Forbered testdata og objekter
+
+            // Opret en instans af KategoriViewModel
+            var viewModel = new KategoriViewModel();
+
+            // Forventede kategorier
+            var expectedKategorier = new List<KategoriModel>
+            {
+                new KategoriModel("0-1 Klasse"),
+                new KategoriModel("2-3 Klasse"),
+                new KategoriModel("4-5 Klasse"),
+                new KategoriModel("6-7 Klasse")
+            };
+
+            // Act: Udfør handlinger på objekterne
+
+            // Hent den faktiske liste af kategorier fra viewModel og konverter den til en liste
+            var actualKategorier = viewModel.Kategorier.ToList();
+
+            // Assert: Bekræft forventede resultater
+
+            // Kontroller, om antallet af forventede kategorier svarer til antallet af faktiske kategorier
+            Assert.Equal(expectedKategorier.Count, actualKategorier.Count);
+
+            // Sammenlign hver forventet kategori med den tilsvarende faktiske kategori
+            for (int i = 0; i < expectedKategorier.Count; i++)
+            {
+                Assert.Equal(expectedKategorier[i].Navn, actualKategorier[i].Navn);
+            }
+        }
+    }
+}
---
+++ UnitTestProjekt/SpilViewModelTests.cs
@@ -0,0 +1,46 @@
+﻿using SpilPlatform.Mvvm.Models;
+using SpilPlatform.Mvvm.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProjekt
+{
+    public class SpilViewModelTests
+    {
+        [Fact]
+        public void SpilViewModel_SetAndGetSpilProperty()
+        {
+            // Arrange: Forbered testdata og objekter
+
+            // Opret en instans af SpilViewModel
+            var viewModel = new SpilViewModel();
+
+            // Opret det forventede Spil-objekt
+            var expectedSpil = new Spil
+            {
+                Titel = "Test Spil",
+                Beskrivelse = "Dette er en testbeskrivelse.",
+                Link = "https://example.com/testgame",
+            };
+
+            // Act: Udfør handlinger på objekterne
+
+            // Indstil Spil-egenskaben i viewModel med det forventede Spil-objekt
+            viewModel.Spil = expectedSpil;
+
+            // Hent det faktiske Spil-objekt fra viewModel
+            var actualSpil = viewModel.Spil;
+
+            // Assert: Bekræft forventede resultater
+
+            // Sammenlign det forventede Spil-objekt med det faktiske Spil-objekt for lighed
+            Assert.Equal(expectedSpil, actualSpil);
+        }
+    }
+}
+
+
+
